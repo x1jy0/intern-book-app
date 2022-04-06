d2479d1f75220af87369de4dadc29cfcc29ebf49
x1jy0 <x1jy0worker@gmail.com>
2022-04-06 19:21:52 +0900

    AngularMaterialの追加,toolbar.componentの作成,list.componentの作成,list.component内にheaderと入力用のformの作成,Routingの作成,

--- intern-book-app/src/app/app.module.ts
+++ intern-book-app/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+import { MatToolbarModule } from'@angular/material/toolbar'
+import { MatFormFieldModule } from'@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select'
+import { MatCardModule } from '@angular/material/card'
+import { MatInputModule } from '@angular/material/input';
+import { ListComponent } from './pages/list/list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+  title='読んだ本';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
