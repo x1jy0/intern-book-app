b03f9a2d1a9ac8ef6f8dcbbb4a25510b7e6b9f9c
x1jy0 <x1jy0worker@gmail.com>
2022-04-12 23:04:57 +0900

    Revert "importのディレクトリを修正,モックデータを入力"
    This reverts commit cf68dae2f5154076ff6285ab4f1d80373571c597.

--- intern-book-app/src/app/pages/list/list.component.ts
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { book } from '../../components/types/book'
+import { book } from '../../book'
 
 @Component({
   selector: 'app-list',
@@ -16,15 +16,4 @@ export class ListComponent implements OnInit {
 
 }
 
-export const bookList: book[] = [
-  {
-    name: 'アンドロイドは電気羊の夢を見るか?',
-    detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが、火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ',
-    evaluation: 90
-  },
-  {
-    name: '岩田さん:岩田聡はこんなことを話していた。',
-    detail: '任天堂の元社長、岩田聡さんのことばをまとめた本',
-    evaluation: 90
-  }
-]
+export const bookList: book[] = []
