5a6e3af5cacfb59dcfcaa49f82b7307416b230c5
x1jy0 <x1jy0worker@gmail.com>
2022-04-20 18:32:23 +0900

    @Inputを記述

--- intern-book-app/src/app/components/card/card.component.ts
+++ intern-book-app/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { Book } from '../../types/book'
 @Component({
   selector: 'app-card',
@@ -6,22 +6,13 @@ import { Book } from '../../types/book'
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  bookList: Book[] = [
-    {
-      name: 'アンドロイドは電気羊の夢を見るか?',
-      detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが、火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ',
-      evaluation: 90
-    },
-    {
-      name: '岩田さん:岩田聡はこんなことを話していた。',
-      detail: '任天堂の元社長、岩田聡さんのことばをまとめた本',
-      evaluation: 90
-    }
-  ]
+  @Input() bookList: Book[] = [];
+  @Input() buttonText: string ='';
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.bookList)
   }
 
 }
