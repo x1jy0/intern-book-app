75081509e83c16fc0ead193123fbdd50baaf1f7f
x1jy0 <x1jy0worker@gmail.com>
2022-04-20 18:34:18 +0900

    app.componentからlist.componentにapp-cardを移動

--- intern-book-app/src/app/app.component.ts
+++ intern-book-app/src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { BlockScrollStrategy } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = '読んだ本';
 }
-
-
-
-
--- intern-book-app/src/app/pages/list/list.component.ts
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { Book } from 'src/app/types/book';
 
 @Component({
   selector: 'app-list',
@@ -7,6 +8,19 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ListComponent implements OnInit {
   title='読んだ本';
+
+  bookList: Book[] = [
+    {
+      name: 'アンドロイドは電気羊の夢を見るか?',
+      detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが、火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ',
+      evaluation: 90
+    },
+    {
+      name: '岩田さん:岩田聡はこんなことを話していた。',
+      detail: '任天堂の元社長、岩田聡さんのことばをまとめた本',
+      evaluation: 90
+    }
+  ]
 
   constructor() { }
 
